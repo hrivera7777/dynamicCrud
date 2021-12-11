0119b2418fd94473ddb7f04bf3f5c8e8484c0bb2
Hans Rivera <hrivera@unal.edu.co>
2021-12-11 14:50:41 -0500

    generalized product card

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -2,50 +2,32 @@
 
 @section('content')
 <div class="container-fluid px-4">
-    <h1 class="mt-4 container">View Products</h1>
+    <h3 class="mt-4 container">View Products</h3>
 
     <div class="row">
-        <div class="col-6">
-    <div class="card mb-1" style="max-width: 480px;">
-        <div class="row g-0">
+        @for ($i = 0; $i < 10; $i++)
+        <div class="col-6 my-2">
+    <div class="card" style="max-height: 240px;">
+        <div class="row g-0" >
           <div class="col-md-4">
             <img
               src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg"
               alt="Trendy Pants and Shoes"
-              class="img-fluid rounded-start"
+              class="img-fluid rounded-start" style="max-height: 230px;"
             />
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h2 class="card-title mb-5">Card title</h2>
               <button type="button" class="btn btn-secondary btn-md mr-5 px-4">Edit</button>
               <button type="button" class="btn btn-danger btn-md px-4">Delete</button>
             </div>
           </div>
         </div>
       </div>
-      
     </div>
-    <div class="col-6">
-        <div class="card mb-1" style="max-width: 480px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img
-                  src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg"
-                  alt="Trendy Pants and Shoes"
-                  class="img-fluid rounded-start"
-                />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <button type="button" class="btn btn-secondary btn-md mr-5 px-4">Edit</button>
-                  <button type="button" class="btn btn-danger btn-md px-4">Delete</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    @endfor
+
     </div>
 
 </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('view');
 });
+
+Route::get('/create', function(){
+    return view('create');
+})->name('create');
